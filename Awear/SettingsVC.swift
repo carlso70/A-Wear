812858d42ea67b5ea8a6e33eff5887bd81e8f2ab
Kathleen Masterson <masterk@purdue.edu>
2018-10-31 01:12:40 -0400

    who knowwsssss

---
+++ Awear/SettingsVC.swift
@@ -0,0 +1,42 @@
+//
+//  SettingsVC.swift
+//  Awear
+//
+//  Created by Kathleen Masterson on 10/31/18.
+//  Copyright © 2018 James Carlson. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import AudioToolbox
+import UIKit
+import AVFoundation
+import CoreAudio
+import CoreLocation
+
+class SettingsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        vibrationSlider.minimumValue = 1;
+//        vibrationSlider.maximumValue = 3;
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    @IBAction func onVibrationChange(_sender: Any)
+//    {
+//        print(vibrationSlider.value)
+//        let sliderValue = lroundf(vibrationSlider.value);
+//        vibrateLvl.text = "\(sliderValue)"
+//        //vibrationSlider.setValue(value: Float(sliderValue), animated: true)
+//
+//        VIBRATION_LEVEL = sliderValue;
+//
+//    }
+}
