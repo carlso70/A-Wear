fe73ecf64d637c613ea7ea7db124fe783eac6fc3
Kathleen Masterson <masterk@purdue.edu>
2018-10-31 22:07:55 -0400

    statistics button and empty page

---
+++ Awear/StatisticsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  StatisticsViewController.swift
+//  Awear
+//
+//  Created by Kathleen Masterson on 10/31/18.
+//  Copyright © 2018 James Carlson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import UserNotifications
+import AudioToolbox
+import UIKit
+import AVFoundation
+import CoreAudio
+import CoreLocation
+
+class StatisticsViewController : UIViewController{
+    
+
+    @IBOutlet weak var but: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //navigationController?.isNavigationBarHidden = false
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func back(_ sender: UIButton){
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+}
