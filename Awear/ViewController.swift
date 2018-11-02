3a24244b7f0db18abfb11a62a6ecc3a638e35bd7
pujamittal <mittal21@purdue.edu>
2018-11-01 21:39:12 -0400

    heart rate in different formats

--- Awear/ViewController.swift
+++ Awear/ViewController.swift
@@ -623,7 +623,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, WCSessionDele
     
     @IBAction func getHeartRate(_ sender: Any) {
         fetchLatestHeartRateSample { (result) in
-            print("\(result?.last?.quantity)\n")
+            //this version gives the values in the form of 00.00 count/min
+//            print("\(String(describing: result?.last?.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))))\n")
+            print("\(String(describing: result?.last?.quantity))\n")
 
         }
     }
