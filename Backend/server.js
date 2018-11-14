89a3cbfda641014e87b3c73b08fa36ef2e354bbc
carlso70 <carlso70@purdue.edu>
2018-11-14 17:10:50 -0500

    db setup for sqlite3

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const app = express();
+const 
+    express = require('express'),
+    app = express(),
+    sqliteDriver = require('./sqliteDriver.js');
 
 app.get('/', (req, res) => {
     res.send('Hello from App Engine!');
+    sqliteDriver.test()
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
