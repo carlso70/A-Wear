ef4d0c05089cc68f02365843331a98ced087811f
carlso70 <carlso70@purdue.edu>
2018-11-15 16:29:57 -0500

    deployed to https://awear-222521.appspot.com/allusers

--- Backend/sqliteDriver.js
+++ Backend/sqliteDriver.js
@@ -3,31 +3,46 @@ const
     db = new sqlite3.Database(':memory:');
 
 module.exports = {
-    initDB: function () {
-        db.serialize(function () {
-            db.run("CREATE TABLE IF NOT EXISTS userTable (username TEXT, password TEXT)");
 
-            var stmt = db.prepare("INSERT INTO userTable (username, password) VALUES (?, ?)");
-            for (var i = 0; i < 10; i++) {
-                stmt.run("Username " + i, "pass " + i);
-            }
+    /* username: String, password: String, isParent: Boolean, child: String */
+    insertUser: function (username, password, isParent, child) {
+        if (child == null) child = "";
+        if (isParent == null) isParent = false;
+
+        db.serialize(() => {
+            var stmt = db.prepare("INSERT INTO userTable (username, password, isParent, child) VALUES (?, ?, ?, ?)");
+            stmt.run(username, password, isParent, child);
             stmt.finalize();
+        })
+    },
 
-            db.each("SELECT rowid AS id, username AS user, password AS pass FROM userTable", function (err, row) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.log(row.id + ": " + row.user + ", " + row.pass);
+    /* Returns a promise with all the users */
+    getAllUsers: () => {
+        return new Promise((resolve, reject) => {
+            db.serialize(() => {
+                db.all("SELECT * FROM userTable", (err, rows) => {
+                    if (err) {
+                        reject(err)
+                    }
+                    resolve(rows);
+                });
             });
         });
     },
 
+    /* Initialize the DB. Creates the user table */
+    initDB: function () {
+        db.serialize(() => {
+            db.run("CREATE TABLE IF NOT EXISTS userTable (username TEXT, password TEXT, isParent INTEGER, child TEXT)");
+        });
+    },
+ 
+    /* Drops the DB. Removes the userTable */
     dropDB: function () {
-        db.serialize(function() {
+        db.serialize(() => {
             db.run("DROP TABLE IF EXISTS userTable");
         });
-    }
+    },
 };
 
 // Checks if a json is empty
