f2ee4e48366361726f8614a04ee80a7a1c873983
carlso70 <carlso70@purdue.edu>
2018-12-03 12:49:56 -0500

    Updates

---
+++ precache-manifest.60ca1be0c003a43c2463e3c1be544376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc7cf60a69eb0dac72f",
+    "url": "/A-Wear/static/js/runtime~main.5cc7cf60.js"
+  },
+  {
+    "revision": "33c4695d371c008fe350",
+    "url": "/A-Wear/static/js/main.33c4695d.chunk.js"
+  },
+  {
+    "revision": "20029dbe4f1811f6ba6b",
+    "url": "/A-Wear/static/js/1.20029dbe.chunk.js"
+  },
+  {
+    "revision": "33c4695d371c008fe350",
+    "url": "/A-Wear/static/css/main.df3a3e3a.chunk.css"
+  },
+  {
+    "revision": "20029dbe4f1811f6ba6b",
+    "url": "/A-Wear/static/css/1.83c48348.chunk.css"
+  },
+  {
+    "revision": "75f83ec28dbf4aa9e301397e6592f9af",
+    "url": "/A-Wear/index.html"
+  }
+];
