b5318cecec879e79a642572d34a56038d2bb376e
carlso70 <carlso70@purdue.edu>
2018-12-02 21:48:04 -0500

    Get user

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,22 @@ const
 app.use(express.json());
 sqliteDriver.initDB();
 
-app.get('/', (req, res) => {
-    res.send('Hello from App Engine!');
-});
-
-// Gets all the users
+/* 
+ * Gets all the users 
+ */
 app.get('/allusers', (req, res) => {
     sqliteDriver.getAllUsers().then(users => res.send(users)).catch(err => res.send(err));
+})
+
+/* 
+ *  Gets a specific users info 
+ */
+app.get('/getuser/:userId', (req, res) => {
+    sqliteDriver.getUser(req.params.userId).then(user => {
+        res.send(user);
+    }).catch(err => {
+        res.send(err);
+    });
 })
 
 /* 
@@ -20,7 +29,8 @@ app.get('/allusers', (req, res) => {
  * body: {username: string, password: string, isParent: bool, child: string}
  */
 app.post('/adduser', (req, res) => {
-    sqliteDriver.insertUser(req.body.username, req.body.password, req.body.isParent, req.body.child);
+    sqliteDriver.insertUser(req.body.username, req.body.password, req.body.isParent,
+         req.body.child, req.body.enabled, req.body.outdoorMode, req.body.recordStats);
     res.sendStatus(200);
 });
 
@@ -30,11 +40,15 @@ app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
 
+/* Testing */
 // sqliteDriver.dropDB();
 // sqliteDriver.initDB();
+
 // for (var i = 0; i < 10; i++) {
-//     sqliteDriver.insertUser("testname " + i, "pass" + i, i % 2 == 0, "testname " + i - 1);
+//     sqliteDriver.insertUser("testname" + i, "pass" + i, i % 2 == 0, "testname " + i - 1);
 // }
+
+// sqliteDriver.getUser("testname0").then(user => console.log(user)).catch(err => console.log("ERR: " + err));
 
 // sqliteDriver.getAllUsers().then(res => {
 //     console.log(res);
