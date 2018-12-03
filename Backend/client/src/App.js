567cb2628b142720f974dc59918c7cf362e746b2
James Carlson <carlso70@purdue.edu>
2018-12-03 11:13:47 -0500

    progress

--- Backend/client/src/App.js
+++ Backend/client/src/App.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
+const awearUrl = "https://awear-222521.appspot.com";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      user: {},
+      pass: "",
+      username: "",
     };
   }
 
+  login() {
+    console.log("Logging in...");
+    console.log(this.state)
+  }
+
+  createAccount() {
+    console.log("Creating account...");
+    fetch(awearUrl + "/adduser", {
+      method: 'post',
+      body: JSON.stringify()
+    }).then(res => {
+
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
-    if (this.state.loggedIn) {
+    if (this.state.loggedIn && this.state.user) {
 
     } else {
       return (
         <div style={{ "margin": "40px"}}>
           <Form>
             <FormGroup>
-              <Label for="Username">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+              <Label for="username">Username</Label>
+              <Input name="username" value={this.state.username} placeholder="Enter Username" />
             </FormGroup>
             <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+              <Label for="password">Password</Label>
+              <Input type="password" name="password" value={this.state.pass} placeholder="Enter Password" />
             </FormGroup>
-            <Button>Login</Button>{' '}
-            <Button>Create Account</Button>
+            <Button onClick={() => this.login()}>Login</Button>{' '}
+            <Button onClick={() => this.createAccount()}>Create Account</Button>
           </Form>
         </div>
       );
