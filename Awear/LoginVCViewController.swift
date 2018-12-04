7a06b7dbb4b0af1d8547750f544651ea2627e8e5
pujamittal <mittal21@purdue.edu>
2018-12-03 23:50:14 -0500

    link to make account

---
+++ Awear/LoginVCViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginVCViewController.swift
+//  Awear
+//
+//  Created by Puja Mittal on 12/3/18.
+//  Copyright © 2018 James Carlson. All rights reserved.
+//
+
+import UIKit
+
+class LoginVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func makeAccount(_ sender: Any) {
+        guard let url = URL(string: "https://stackoverflow.com") else { return }
+        UIApplication.shared.open(url)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
