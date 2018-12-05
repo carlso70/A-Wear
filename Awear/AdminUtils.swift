520bbc70ba8831d8228003b8bfd86069651ebb60
carlso70 <carlso70@purdue.edu>
2018-12-05 13:14:37 -0500

    settings now are set correctly upon logging in. TODO: every time view controller loads, make a network request to get the user and update the new settings

--- Awear/AdminUtils.swift
+++ Awear/AdminUtils.swift
@@ -12,7 +12,8 @@ class AdminUtils {
     static func updateSettings(response: NSDictionary) {
         UserDefaults.standard.set(response.object(forKey: "recordStats") as! Bool, forKey: "recordStats")
         UserDefaults.standard.set(response.object(forKey: "enabled") as! Bool, forKey: "audioEnabled")
-        UserDefaults.standard.set(response.object(forKey: "outdoorMode") as! Bool, forKey: "outdoorManEnabled")
+        UserDefaults.standard.set(response.object(forKey: "outdoorMode") as! Bool, forKey: "outdoorManEnable")
+        UserDefaults.standard.set(response.object(forKey: "outdoorMode") as! Bool, forKey: "outdoorAutoEnable")
         UserDefaults.standard.set(response.object(forKey: "username") as! String, forKey: "username")
         UserDefaults.standard.set(response.object(forKey: "child") as! String, forKey: "child")
     }
