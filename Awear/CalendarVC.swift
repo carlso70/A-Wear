512d8f96bd27d1d14b9afa4c3631229ee80ea665
carlso70 <carlso70@purdue.edu>
2018-12-06 00:54:35 -0500

    maybe

--- Awear/CalendarVC.swift
+++ Awear/CalendarVC.swift
@@ -17,23 +17,23 @@ import CoreLocation
 import EventKit
 
 
-class CalendarVC : UIViewController{
+class CalendarVC : UIViewController,  UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var backBtn: UIButton!
     
-   // var cal;
+    // var cal;
     var events: [EKEvent] = [];
     @IBOutlet weak var tableView: UITableView!
+    
+    var test: [String] = ["test", "test1", "hiKatie"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let store = EKEventStore()
         
-        
         // Get the appropriate calendar
         let calendar = Calendar.current
-        
         
         if store.responds(to: #selector(EKEventStore.requestAccess(to:completion:))) {
             // iOS Settings > Privacy > Calendars > MY APP > ENABLE | DISABLE
@@ -56,11 +56,9 @@ class CalendarVC : UIViewController{
                     // Fetch all events that match the predicate
                     self.events = store.events(matching: predicate)
                     print("The content of array is\(self.events)")
-                    }
+                }
             }
         }
-
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -69,32 +67,27 @@ class CalendarVC : UIViewController{
     }
     
     @IBAction func back(_ sender: Any) {
-         self.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
-    
-    //MARK: UITableViewDataSource
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //TEST PRINT EVENT COUNT
-        print("event count (print) \(events.count)")
-        
-        
-        return events.count 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //        return events.count
+        return test.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cellIdentifier = "cell"
-        let cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)
-        let events:EKEvent! = self.events[indexPath.row] 
-        cell.textLabel!.text = events.title
-        cell.detailTextLabel!.text = events.startDate.description
-        print("event cell returned")
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        cell.textLabel?.text = test[indexPath.row]
+//        let events = self.events[indexPath.row]
+//        cell.textLabel?.text = events.title
+//        cell.detailTextLabel!.text = events.startDate.description
+//        print("event cell returned")
         return cell
     }
-    
 }
