db8f956d98a548a0399d2acf406c5fe20161e8e2
Kathleen Masterson <masterk@purdue.edu>
2018-12-06 23:59:42 -0500

    fix what jimmy broke

--- Awear/AdminUtils.swift
+++ Awear/AdminUtils.swift
@@ -13,7 +13,7 @@ class AdminUtils {
         UserDefaults.standard.set(response.object(forKey: "recordStats") as! Bool, forKey: "recordStats")
         UserDefaults.standard.set(response.object(forKey: "enabled") as! Bool, forKey: "audioEnabled")
         UserDefaults.standard.set(response.object(forKey: "outdoorMode") as! Bool, forKey: "outdoorManEnable")
-        UserDefaults.standard.set(response.object(forKey: "outdoorMode") as! Bool, forKey: "outdoorAutoEnable")
+//        UserDefaults.standard.set(response.object(forKey: "outdoorMode") as! Bool, forKey: "outdoorAutoEnable")
         UserDefaults.standard.set(response.object(forKey: "username") as! String, forKey: "username")
         UserDefaults.standard.set(response.object(forKey: "child") as! String, forKey: "child")
         UserDefaults.standard.set(response.object(forKey: "password") as! String, forKey: "password")
